bed46c87981dbe4e65f4d462e57688acc746e83c
pantpad <138708449+pantpad@users.noreply.github.com>
2024-01-14 15:43:01 +0100

    Generate initial UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,44 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Ciao!</h1>
+      <main>
+        <section className="game-container">
+          <div className="players-container">
+            <div className="player">
+              <span>player1</span>
+              {/* <input type="text" placeholder="player1" /> */}
+              <div>
+                <p>X</p>
+                <button>Edit</button>
+              </div>
+            </div>
+            <div className="player">
+              <span>player2</span>
+              {/* <input type="text" placeholder="player2" /> */}
+              <div>
+                <p>O</p>
+                <button>Edit</button>
+              </div>
+            </div>
+          </div>
+          <div className="game-board-container">
+            <div className="game-board">
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+              <button className="squared-button">x</button>
+            </div>
+          </div>
+        </section>
+        <section className="game-logs">
+          <h1>gameLogs</h1>
+        </section>
+      </main>
     </>
   );
 }
