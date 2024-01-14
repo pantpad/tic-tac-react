090351085c5eb7a4b85bcdce673bde83e6954ff2
pantpad <138708449+pantpad@users.noreply.github.com>
2024-01-14 16:56:26 +0100

    Allow players to be edited,saved and resetted

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import "./App.css";
+import { useState } from "react";
 import Player from "./components/Player/Player";
 
-let players = [
+const PLAYERS = [
   { name: "PLAYER1", symbol: "X" },
   { name: "PLAYER2", symbol: "O" },
 ];
 
 function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  function changePlayers(newName, playerSymbol) {
+    setPlayers((prevPlayers) => {
+      return [
+        ...prevPlayers.map((player) => {
+          if (player.symbol === playerSymbol) {
+            return { ...player, name: newName.toUpperCase() };
+          } else {
+            return { ...player };
+          }
+        }),
+      ];
+    });
+  }
+
+  function resetGame() {
+    console.log("Game Will Be RESET!");
+    setPlayers(PLAYERS);
+  }
+
   return (
     <>
       <main>
         <section className="game-container">
           <div className="players-container">
-            <Player {...players[0]} />
-            <Player {...players[1]} />
+            <Player {...players[0]} onSave={changePlayers} />
+            <Player {...players[1]} onSave={changePlayers} />
           </div>
           <div className="game-board-container">
             <div className="game-board">
@@ -31,6 +52,7 @@ function App() {
         </section>
         <section className="game-logs">
           <h1>gameLogs</h1>
+          <button onClick={resetGame}>RESET GAME</button>
         </section>
       </main>
     </>
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, onSave }) {
+  const [inputName, setInputName] = useState();
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((prevEdit) => !prevEdit);
+    setInputName(name);
+  }
+
+  function handleChange(e) {
+    setInputName(e.target.value);
+  }
+
+  function handleSave() {
+    setIsEditing((prevEdit) => !prevEdit);
+    onSave(inputName, symbol);
+  }
+
+  //prettier-ignore
+  let playerSection = !isEditing ? <span>{name}</span> : <input type="text" value={inputName} onChange={handleChange} required/>;
+
+  //prettier-ignore
+  let buttonSection = !isEditing ? <button onClick={handleEditClick}>Edit</button> : <button onClick={handleSave}>Save</button>;
+
   return (
     <>
       <div className="player">
-        <span>{name}</span>
-        {/* <input type="text" placeholder="player1" /> */}
+        {playerSection}
         <div>
           <p>{symbol}</p>
-          <button>Edit</button>
+          {buttonSection}
         </div>
       </div>
     </>
